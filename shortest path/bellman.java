69c0ba618f8b957386eca7c0bc5f3b6bfdf12705
Mansi Agrawal <107387619+mansi-ag25@users.noreply.github.com>
2023-07-04 07:07:33 +0530

    Create bellman.java

---
+++ shortest path/bellman.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+
+//belman will fail in the case of negative weight cycle and this can be checked by again running the 
+// loop and checking if the values are getting updated or not, if yes then it contains negative 
+// weight cycle.
+
+
+public class bellman {
+
+    static class Edge{
+    int src;
+    int dest;
+    int wt;
+    public Edge(int s,int d,int w){
+        this.src=s;
+        this.dest=d;
+        this.wt=w;
+
+    }
+}
+
+    public static void create(ArrayList<Edge> graph[]){
+        for (int i=0;i<graph.length;i++){
+            graph[i]=new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0,1,2));
+        graph[0].add(new Edge(0,2,4));
+
+        graph[1].add(new Edge(1,2,-4));
+
+        graph[2].add(new Edge(2,3,2));
+
+        graph[3].add(new Edge(3,4,4));
+
+        graph[4].add(new Edge(4,1,-1));
+
+
+    }
+
+
+    public static void bellmanAlgo(ArrayList<Edge> graph[],int src,int v){
+        int dist[]=new int[v];
+        for (int i=0;i<v;i++){
+            if(i!=src){
+                dist[i]=Integer.MAX_VALUE;
+            }
+
+        }
+        for (int k=0;k<v-1;k++){
+            for (int i=0;i<v;i++){
+                for (int j=0;j<graph[i].size();j++){
+                    Edge e=graph[i].get(j);
+                    int u=e.src;
+                    int V=e.dest;
+                    if(dist[u]!=Integer.MAX_VALUE && dist[u]+e.wt<dist[V]){
+                        dist[V]=dist[u]+e.wt;
+                    }
+                }
+            }
+        }
+
+
+        for (int i=0;i<dist.length;i++){
+            System.out.print(dist[i]+" ");
+        }
+    }
+    public static void main(String args[]){
+        int v=5;
+        ArrayList<Edge> graph[]=new ArrayList[v];
+        create(graph);
+        bellmanAlgo(graph,0,v);
+    }
+}
